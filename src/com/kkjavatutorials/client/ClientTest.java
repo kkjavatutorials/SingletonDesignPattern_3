8365c66b5015ccfef95ebed8ff8f86ce50fef6dc
Kishan Kumar <64035167+kkjavatutorials@users.noreply.github.com>
2020-04-27 11:56:54 +0530

    Update ClientTest.java

--- src/com/kkjavatutorials/client/ClientTest.java
+++ src/com/kkjavatutorials/client/ClientTest.java
@@ -1,7 +1,7 @@
 package com.kkjavatutorials.client;
 
 import com.kkjavatutorials.singleton.Singleton;
-// KK JavaTutorials-update
+// KK JavaTutorials-update new
 public class ClientTest {
 
 	public static void main(String[] args) {
